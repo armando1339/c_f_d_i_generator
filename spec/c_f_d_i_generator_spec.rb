519168539f6198f7bf475b461f67bd3d4a38508a
Armando Alejandre <armandoalejandre@Miguels-MacBook-Pro.local>
2017-07-03 17:50:41 -0500

    Update README 2

--- spec/c_f_d_i_generator_spec.rb
+++ spec/c_f_d_i_generator_spec.rb
@@ -1,16 +1,10 @@
 require "spec_helper"
 
-RSpec.describe CFDIGenerator do
-  it "has a version number" do
-    expect(CFDIGenerator::VERSION).not_to be nil
+describe Hello do 
+  context "first test" do
+    it "is a test" do 
+      @hello = Hello.new
+      expect(@hello.pow(3, 2)).to eql(9)
+    end
   end
-
-  describe Hello do 
-	  context "first test" do
-	    it "is a test" do 
-	      @hello = Hello.new
-	      expect(@hello.pow(3, 2)).to eql(9)
-	    end
-	  end
-	end
 end
