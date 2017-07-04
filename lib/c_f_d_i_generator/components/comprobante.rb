7af3b5991706c717a49964fea679f1f0f0fc3e73
Armando Alejandre <armandoalejandre@Miguels-MacBook-Pro.local>
2017-07-04 15:08:53 -0500

    Build comprobante component

--- lib/c_f_d_i_generator/components/comprobante.rb
+++ lib/c_f_d_i_generator/components/comprobante.rb
@@ -90,7 +90,7 @@ module CFDIGenerator
 				scheme = {}
 				scheme.merge! CONSTANTS_IN_THE_SCHEME
 				@@scheme.each do |k,v|
-					scheme[k] = self.instance_variable_get("@#{v}") if self.respond_to?(v) and self.instance_variable_get("@#{v}").blank?
+					scheme[k] = self.instance_variable_get("@#{v}") if self.respond_to?(v) and !self.instance_variable_get("@#{v}").blank?
 				end
 				return scheme
 			end
